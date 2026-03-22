7eedf5ba41c41e918c9ec89feac1535935378196
vnehaenterprises-bit <v.nehaenterprises@gmail.com>
2026-03-22 10:13:10 +0530

    Delete backend directory

--- backend/routes/videoRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { uploadVideo, getVideos } = require('../controllers/videoController');
-
-const router = express.Router();
-
-router.post('/', uploadVideo);
-router.get('/', getVideos);
-
-module.exports = router;
